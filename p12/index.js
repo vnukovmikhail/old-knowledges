3787100c003e9481678f1765ba58c5e7b8ea36b0
k7q9s <vnukovmisa72@gmail.com>
2024-11-11 22:59:20 +0200

    go

--- p12/index.js
+++ p12/index.js
@@ -1,13 +1,30 @@
 const section = document.querySelector('section')
+section.style.display = 'flex';
 const h1 = document.createElement('h1')
 h1.textContent = 'No Way!'
-h1.style.cssText = ``;
+h1.style.cssText = `
+    align-content: center;
+    justify-content: center;
+    color: blue;
+    background: royalblue;
+    padding: 0 10px;
+`;
 const ul = document.createElement('ul')
+ul.style.padding = '0';
+ul.style.listStyle = 'none';
 ul.innerHTML = `
     <li>000</li>
     <li>123</li>
     <li>321</li>
 `;
+for (let i = 0; i < ul.children.length; i++) {
+    const li = ul.children[i]
+    li.style.cssText = `
+        background: black;
+        color: white;
+        margin: 5px;
+    `;
+}
 section.appendChild(h1)
 section.appendChild(ul)
 document.body.appendChild(section)
